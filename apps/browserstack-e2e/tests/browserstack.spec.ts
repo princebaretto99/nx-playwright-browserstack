a137e63b11fd20782a813f70aff0ff47a30907c4
racheljose21 <racheljose21@gmail.com>
2025-11-11 23:50:17 +0530

    legacy playwright integration

--- apps/browserstack-e2e/tests/browserstack.spec.ts
+++ apps/browserstack-e2e/tests/browserstack.spec.ts
@@ -1,6 +1,7 @@
-import { test, expect } from '@playwright/test';
+import { expect, Page } from '@playwright/test';
+const { test } = require('../fixture');
 
-test('visits browserstack.com and validates title', async ({ page }) => {
+test('visits browserstack.com and validates title', async ({ page }: { page: Page }) => {
   await page.goto('https://www.browserstack.com/');
   await expect(page).toHaveTitle(/BrowserStack/i);
   await page.waitForTimeout(10000);
